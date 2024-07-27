4615832b46e9c3e1930aefcbbe96fa14a5f25d21
fabruizb <166636528+fabruizb@users.noreply.github.com>
2024-07-27 20:11:27 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea] = useState("");
+	const [tareas, setTareas] = useState([]);
+	const [hoveredIndex, setHoveredIndex] = useState(null);
+
+	const agregarTarea = () => {
+		if (tarea.trim()) {
+			setTareas((prev) => [...prev, tarea]);
+			setTarea("");
+		}
+	};
+
+	const eliminarTarea = (index) => {
+		setTareas((prev) => prev.filter((_, i) => i !== index));
+	};
+
+	const handleKeyDown = (evento) => {
+		if (evento.key === "Enter") {
+			agregarTarea();
+		}
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="list" >
+			<h1 className="title">ToDo List</h1>
+			<div className="card">
+				<input
+					className="input"
+					type="text"
+					value={tarea}
+					onChange={(evento) => { setTarea(evento.target.value) }}
+					onKeyDown={handleKeyDown} 
+					placeholder="What need to be done?"
+					/>
+
+
+				<ul className="task-list">
+					{tareas.map((posicion, index) => (
+						<li
+							key={index}
+							onMouseEnter={() => setHoveredIndex(index)}
+							onMouseLeave={() => setHoveredIndex(null)}
+							className="task-item"
+						>
+							{posicion}{""}
+							{hoveredIndex === index && (
+								<button className="delete" onClick={() => eliminarTarea(index)}><i class="fa-solid fa-xmark"></i></button>
+							)}
+						</li>
+					))}
+				</ul>
+				<div class="footer">
+					{tareas.length} {tareas.length === 1 ? "item" : "items"} left
+					
+				</div>
+			</div>
 		</div>
 	);
 };
